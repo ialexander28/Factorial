d9e5cb6484e429fcb671d92840fa40a468dd3e66
ialexander28 <buildingbusinessideas@gmail.com>
2021-04-18 16:06:12 -0600

    new

--- n_k_v2.cpp
+++ n_k_v2.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <math.h>
 
+// repo toke: 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
+
 int factorial(int num); 
 
 int main() 
 {
     int n, k, k_comb; 
     int nFact, kFact, n_kFact;
+    int kCombinations(int n, int k); 
 
     std::cout << '\n';
 
     std::cout << "Enter n and k (n must be > or = k): " << '\n';
     std::cin >> n >> k; 
+
+    k_comb = kCombinations(n, k); 
+
+    std::cout << " Result of 'n' choose 'k' is: " << k_comb << '\n';
+
+    std::cout << '\n';
+
+return 0; 
+}
+
+int kCombinations (int n, int k){
+
+    int nFact, kFact, n_kFact, k_comb;
 
     nFact = factorial(n); 
 
@@ -19,13 +35,7 @@ int main()
 
     n_kFact = factorial(n - k); 
     
-    k_comb = (nFact / (kFact * n_kFact));
-
-    std::cout << " Result of 'n' choose 'k' is: " << k_comb << '\n';
-
-    std::cout << '\n';
-
-return 0; 
+    return k_comb = (nFact / (kFact * n_kFact));
 }
 
 int factorial(int num){

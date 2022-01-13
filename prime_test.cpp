e36cd24de25e6534443910c8fdb864b9edd4cdd9
ialexander28 <buildingbusinessideas@gmail.com>
2022-01-13 16:45:01 -0700

    prime_test function

---
+++ prime_test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+bool isPrime(int num) {
+
+    int countDivs; 
+    countDivs = 0; 
+    for(int i=1; i <= num; i++){
+        if(num % i == 0)
+            countDivs++;
+    }
+    if(countDivs == 2) 
+        return true; 
+    else
+        return false; 
+}
+
+int main()
+{ 
+    int num;   
+    std::cout << "Enter a number " << '\n'; 
+
+    std::cin >> num;
+
+    isPrime(num);
+
+    if(isPrime(num) == true)
+        std::cout << "The number is Prime! " << '\n';
+    else
+        std::cout << "The number is not Prime! " << '\n';
+
+    return 0; 
+}
